ac07a96693b1e40029814e4a5f7ca2168b206f8f
Jaswir <jaswirraghoe@gmail.com>
2025-02-15 23:58:41 +0100

    powered by deepseek

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,7 +39,7 @@ export function Footer() {
                 <Link
                   key={index}
                   href="/"
-                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
+                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-red-500 focus:text-red-500 focus:bg-red-100 focus:outline-none dark:focus:bg-trueGray-700"
                 >
                   {item}
                 </Link>
@@ -52,7 +52,7 @@ export function Footer() {
                 <Link
                   key={index}
                   href="/"
-                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
+                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-red-500 focus:text-red-500 focus:bg-red-100 focus:outline-none dark:focus:bg-trueGray-700"
                 >
                   {item}
                 </Link>
